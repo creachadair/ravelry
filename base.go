e17195363fa3306c8f947d47c3529dd169528f56
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-05-01 08:42:19 -0700

    WIP: fold types.go into base.go

--- base.go
+++ base.go
@@ -8,7 +8,10 @@ package ravelry
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 )
 
 // HTTPClient is the interface required to call the Ravelry API service.
@@ -43,4 +46,42 @@ func WithBasicAuth(ctx context.Context, user, token string) context.Context {
 		Type:  "Basic",
 		Token: base64.StdEncoding.EncodeToString([]byte(auth)),
 	})
+}
+
+// TimeFormat is the timestamp format used by the Ravelry API.
+const TimeFormat = "2006/01/02 15:04:05 -0700"
+
+// A Timestamp represents a timestamp as encoded by the Ravelry API.
+type Timestamp time.Time
+
+func (ts *Timestamp) UnmarshalJSON(data []byte) error {
+	var stamp string
+	if err := json.Unmarshal(data, &stamp); err != nil {
+		return fmt.Errorf("incorrect timestamp format: %v", err)
+	}
+	v, err := time.Parse(TimeFormat, stamp)
+	if err != nil {
+		return fmt.Errorf("invalid timestamp: %v", err)
+	}
+	*ts = Timestamp(v)
+	return nil
+}
+
+func (ts Timestamp) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + time.Time(ts).Format(TimeFormat) + `"`), nil
+}
+
+// PageInfo records the request  parameters controlling pagination.
+type PageInfo struct {
+	Page     int `json:"page,omitempty"`
+	PageSize int `json:"page_size,omitempty"`
+}
+
+// A Paginator is sent in replies large enough to require pagination.
+type Paginator struct {
+	PageCount int `json:"page_count"`
+	Page      int `json:"page"`
+	PageSize  int `json:"page_size"`
+	Results   int `json:"results"`
+	LastPage  int `json:"last_page"`
 }
