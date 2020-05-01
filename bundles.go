3ce1a789e54e5bbf140a320033637ef776b36aa2
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-05-01 09:35:39 -0700

    WIP: start adding bundles methods

---
+++ bundles.go
@@ -0,0 +1,15 @@
+package ravelry
+
+import (
+	"context"
+
+	"github.com/creachadair/ravelry/types"
+)
+
+func (c *Client) CreateBundle(ctx context.Context, user string, req types.BundlePost) (*types.Bundle, error) {
+	return nil, nil
+}
+
+func (c *Client) DeleteBundle(ctx context.Context, user string, id int) (*types.Bundle, error) {
+	return nil, nil
+}
